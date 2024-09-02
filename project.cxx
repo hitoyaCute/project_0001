1a66d15f2bd6cf6073a65f2494d983c3b33a0cc6
hitoyaCute <147114029+hitoyaCute@users.noreply.github.com>
2024-09-02 19:25:46 +0800

    Create project.cxx

---
+++ project.cxx
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int cm;
+  cout << "convert cm to m\nplease enter a whole number:";
+  cin >> cm;
+  int m = cm/100;
+  cout << "\nresult is:" << m;
+}
